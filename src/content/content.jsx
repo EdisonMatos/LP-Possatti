d935e4d01b61dafac42053b1bbc5ea2a5d27402e
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-11 18:04:41 -0300

    fix: Foco no copy da Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -127,8 +127,8 @@ const content = {
       miniTag: "ESCOLA DE JIU-JITSU EM VILA VELHA - ES",
       title: (
         <h1 className="mb-[16px]">
-          Jiu-Jitsu para todos os níveis
-          <span className="text-primary"></span>
+          Jiu-Jitsu
+          <span className="text-primary"> para todos </span>os níveis
         </h1>
       ),
       subtitle:
