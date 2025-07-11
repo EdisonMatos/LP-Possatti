cd3b503fe3bd5b390a3faad5a7ef06fbbf950195
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-11 17:06:49 -0300

    fix: Features

--- src/components/sectionElements/hero/Influencer.jsx
+++ src/components/sectionElements/hero/Influencer.jsx
@@ -103,7 +103,7 @@ export default function Influencer({ appDownloadButtons }) {
                 </div>
               </div>
               <div className="flex justify-center w-full tablet1:w-full desktop1:w-[35%] ">
-                <MotionDivRightToLeft className="relative flex items-end justify-center w-full desktop2:justify-end">
+                <MotionDivRightToLeft className="relative mb-12 flex items-end justify-center w-full desktop2:justify-end">
                   <img
                     src={content.texts.hero.heroImgPhone}
                     className="h-[100%] relative desktop1:hidden"
